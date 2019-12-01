45c2012f914c2e874d2af57e22bd600f5bd682f5
Tiago Horta <tiago_horta95@hotmail.com>
2019-12-01 12:09:06 +0000

    TS: Add project folder structure

---
+++ my-app/src/index.tsx
@@ -0,0 +1,7 @@
+// Modules
+import React from "react";
+import ReactDOM from "react-dom";
+// Components
+import Home from "./scenes/Home";
+
+ReactDOM.render(<Home />, document.getElementById("root"));
---
+++ my-app/src/scenes/Home.tsx
@@ -0,0 +1,12 @@
+// Modules
+import React from "react";
+
+const Home: React.FunctionComponent = () => {
+    return (
+        <div className="Home">
+            <p>Hello World !</p>
+        </div>
+    );
+};
+
+export default Home;
