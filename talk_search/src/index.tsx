b4cd6d37910cf2e72eae330539a8e79b875b43c3
Tiago Horta <tiago_horta95@hotmail.com>
2019-12-03 21:25:15 +0000

    TS_01: Add top bar

--- talk_search/src/index.tsx
+++ talk_search/src/index.tsx
@@ -3,5 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 // Components
 import Home from "./scenes/Home";
+// Styles
+import 'semantic-ui-css/semantic.min.css'
 
 ReactDOM.render(<Home />, document.getElementById("root"));
